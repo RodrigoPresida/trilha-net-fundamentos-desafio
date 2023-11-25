a00e6dca2b4d9877360f72a638e0ff4aaf61cd27
Rodrigo Presida <rodrigopresida@gmail.com>
2023-11-25 16:53:06 -0300

    modificando

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -16,7 +16,12 @@ namespace DesafioFundamentos.Models
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
+
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            Console.ReadLine();
+            string veiculos = Console.ReadLine();
+           
+            
         }
 
         public void RemoverVeiculo()
